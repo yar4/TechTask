c0e3b9f0508b1311a05179c55730222697b392bb
yar4 <yaro93@live.ru>
2018-03-13 21:04:19 +0200

    function with 3 arguments

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,32 @@
-number1 = 5
-number2 = 10
-number3 = 2
 
 
-def handle_numbers (number1,number2,number3):
-    a = range(number1,number2,number3)
-    print(a)
+def handle_number(number1,number2,number3):#function takes 3 arguments
+    division = list(range(number1,number2))#convert to list
+    counting = 0#score counter
+    for result in division[:]:
+        a = result % number3 #we find division without rest
+        try:
+            if a == 0:
+                counting+=1#set score
+        except:
+            print('Incorect input')
+    return counting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
