5e6370599292937daa4cfe346a68af00cb04c726
yar4 <yaro93@live.ru>
2018-03-13 23:16:19 +0200

    Task3

--- Assignment3.py
+++ Assignment3.py
@@ -1,17 +1,10 @@
-class Person:
-    def __init__(self,name,age,height,weight):
-        self.name = name
-        self.age = age
-        self.height = height
-        self.weight = weight
-    def __repr__(self):
-        return repr((self.name, self.age, self.height, self.weight))
-
-    list = [
-    ("Tom", "19", "167", "54"),
-    ("Jony", "24", "180", "69"),
-    ("Json", "21", "185", "75"),
-    ("John", "27", "190", "87"),
-    ("Jony", "24", "191", "98"),
+import operator
+Person = [
+("Tom", "19", "167", "54"),
+("Jony", "24", "180", "69"),
+("Json", "21", "185", "75"),
+("John", "27", "190", "87"),
+("Jony", "24", "191", "98"),
     ]
-sorted(list,key=lambda person: Person.age)
+
+print(sorted(Person, key=operator.itemgetter(0,1,2,3)))
