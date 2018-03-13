9c3b751f483510ad85e2b31f4a1be0e81b149ea2
yar4 <yaro93@live.ru>
2018-03-13 22:16:17 +0200

    task2

---
+++ Assignment2.py
@@ -0,0 +1,25 @@
+s = input('Enter string')
+
+
+def string_handler(s):
+    numbers = 0
+    letters = 0
+    for i in s:
+        while True:
+            try:
+                if i.isdigit()is True:
+                    numbers+=1
+                break
+            except Exception as e:
+                print(e)
+        try:
+            if i.isalpha() == True:
+                letters+=1
+        except Exception as e:
+            print(e)
+    return (numbers)
+
+
+
+
+
